20a431cfbc0bbfd1e574ec54df6dd15589fcdc9b
hellno <hellno@users.noreply.github.com>
2025-06-19 18:50:02 +0200

    Update route.ts

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -4,21 +4,25 @@ export async function GET() {
     accountAssociation: {
       header:
         "eyJmaWQiOjEzNTk2LCJ0eXBlIjoiY3VzdG9keSIsImtleSI6IjB4ODE3MzE4RDZmRkY2NkExOGQ4M0ExMzc2QTc2RjZlMzBCNDNjODg4OSJ9",
-      payload:
-        "eyJkb21haW4iOiJkYXBwY29uLXdvcmtzaG9wLXRlbXBsYXRlLnZlcmNlbC5hcHAifQ",
+      payload: "eyJkb21haW4iOiJkYXBwY29uLWNpcmNsZXMtZGVtby52ZXJjZWwuYXBwIn0",
       signature:
-        "MHg0M2ZhZTNlMTk1ZjU0ZDlmMmVlOTM2OGQyN2JjNDIxNGNlODdkOWYyYmE0ZGQxYjg0NGYxOWRiNzMwYTMwNWEyMDljY2E3MDczYThkYjVlMjFiNWJlNTY3N2VkNjIwOTEwZTFkZTRhOGM3MzczZWRkMzUxZTZhNWYwYzA3ZmZjNjFj",
+        "MHgzZGFjZDYxNTUwNzRkMzdhZDkzNmI1OWY5M2FkMzQzYTA0N2Q0NDU2OWI2ZDdhZDM3MWNlYWZhMjRkODM1YjRlMDE2YWFkZDBjYTg1MDQyZTQxZDM0YjAwNjU4NWZiNzQ0NjFmMzAwNDUyYTk3ZTBkZWQ5M2NjNzExNmY4Yjc5YTFi",
     },
     frame: {
       version: "1",
-      name: "DappCon Mini App Template",
+      name: "DappCon Circles Demo",
+      subtitle: "find farcaster friends on circles",
+      tagline: "find farcaster friends on circles",
+      description: "help you find farcaster friends on circles",
       iconUrl: `${appUrl}/icon.png`,
       homeUrl: appUrl,
-      imageUrl: `${appUrl}/frames/hello/opengraph-image`,
-      buttonTitle: "Launch Frame",
+      imageUrl: `${appUrl}/opengraph-image`,
+      buttonTitle: "Launch",
       splashImageUrl: `${appUrl}/splash.png`,
       splashBackgroundColor: "#f7f7f7",
       webhookUrl: `${appUrl}/api/webhook`,
+      primaryCategory: "productivity",
+      tags: ["circles", "friends", "gnosis", "social"],
     },
   };
 
